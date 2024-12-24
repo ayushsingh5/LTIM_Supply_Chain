cbecc70c71f9fbb29467094c6d4dcedc1c691090
username <wecp-dummy@gmail.com>
2024-12-24 12:51:11 +0000

    wecp initial commit

---
+++ backend/src/main/java/com/wecp/progressive/dao/SupplierDAOImpl.java
@@ -0,0 +1,35 @@
+package com.wecp.progressive.dao;
+
+import java.util.List;
+
+import com.wecp.progressive.entity.Supplier;
+
+public class SupplierDAOImpl {
+
+    @Override
+    public int addSupplier(Supplier supplier){
+        return -1;
+    }
+
+    @Override
+    public Supplier getSupplierById(int supplierId){
+        return null;
+    }
+
+    @Override
+    public void updateSupplier(Supplier supplier){
+
+    }
+
+    @Override
+    public void deleteSupplier(int supplierId){
+
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliers(){
+        return List.of();
+    }
+
+
+}
---
+++ backend/src/main/java/com/wecp/progressive/dao/WarehouseDAOImpl.java
@@ -0,0 +1,33 @@
+package com.wecp.progressive.dao;
+
+import java.util.List;
+
+import com.wecp.progressive.entity.Warehouse;
+
+public class WarehouseDAOImpl {
+
+    @Override
+    int addWarehouse(Warehouse warehouse){
+        return -1;
+    }
+
+    @Override
+    public Warehouse getWarehouseById(int warehouseId){
+        return null;
+    }
+
+    @Override
+    public void updateWarehouse (Warehouse warehouse){
+
+    }
+
+    @Override
+    public void deleteWarehouse (int warehouseId){
+
+    }
+
+    @Override
+    public List<Warehouse> getAllWarehouse(){
+        return List.of();
+    }
+}
